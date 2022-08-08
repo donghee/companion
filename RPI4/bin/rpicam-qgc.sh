d9224902496e3aad63ff2946586cae66b4918510
Donghee Park <dongheepark@gmail.com>
2022-08-08 17:25:25 +0900

    Update

--- RPI4/bin/rpicam-qgc.sh
+++ RPI4/bin/rpicam-qgc.sh
@@ -5,7 +5,7 @@ x=1
 while [ $x -le 100 ]
 do
   echo "try connect to GCS: $x times"
-  gst-launch-1.0 rpicamsrc bitrate=2000000 ! 'video/x-h264,width=720,height=480' ! h264parse ! queue ! rtph264pay config-interval=1 pt=96 ! udpsink host=$QGC_IP port=5600
+  gst-launch-1.0 rpicamsrc bitrate=2000000 vflip=false hflip=false ! 'video/x-h264,width=720,height=480' ! h264parse ! queue ! rtph264pay config-interval=1 pt=96 ! udpsink host=$QGC_IP port=5600
 
   x=$(( $x + 1 ))
   sleep 1
