1e073c2ecade6b568002889e51d74121a4ba4c39
Donghee Park <dongheepark@gmail.com>
2022-08-30 19:17:25 +0900

    Update local rtsp simple server

--- GCS/tmux-server.sh
+++ GCS/tmux-server.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 tmux new -d -s $USER
+tmux new-window -n "raspberrypi"
+tmux send-keys -t "$USER:raspberrypi" 'ssh pi@223.171.56.36' Enter
 tmux new-window -n "mavproxy"
-tmux send-keys -t "$USER:mavproxy" 'mavproxy.py --master=tcp:223.171.56.36:5760 --master=/dev/ttyUSB0 --out=udp:0.0.0.0:14550 --console --map --load-module=horizon' Enter
+tmux send-keys -t "$USER:mavproxy" 'mavproxy.py --master=tcp:223.171.56.36:5760 --master=tcp:192.168.100.101:5760 --out=udp:0.0.0.0:14550 --console --map --load-module=horizon' Enter
 tmux new-window -n "joystick"
-tmux send-keys -t "$USER:joystick" 'cd scripts; python3 virtual-joy.py' Enter
+tmux send-keys -t "$USER:joystick" 'cd scripts && sudo chmod 666 /dev/uinput && python3 virtual-joy.py' Enter
 tmux new-window -n "streaming"
 tmux send-keys -t "$USER:streaming" 'cd rtsp-server; ./run.sh' Enter
 tmux new-window -n "detect"
-tmux send-keys -t "$USER:detect" 'cd object-detect; ./run.sh' Enter
+tmux send-keys -t "$USER:detect" 'cd object-detect; ./run.sh' 
